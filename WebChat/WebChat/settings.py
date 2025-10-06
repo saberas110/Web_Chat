220c84e41b85f42c62ed3d5c2673ea2fed75189f
saber <saberas110@gmail.com>
2025-10-06 11:30:16 +0330

    completed migrations contact_userprofile_userprofileimages.py

--- WebChat/WebChat/settings.py
+++ WebChat/WebChat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from datetime import timedelta
 
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.User'
 
--- WebChat/chat/admin.py
+++ WebChat/chat/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Conversation, Message
+from .models import Conversation, Message, UserProfile,UserProfileImages,Contact
+
+
+
 admin.site.register(Conversation)
 admin.site.register(Message)
+admin.site.register(UserProfile)
+admin.site.register(UserProfileImages)
+admin.site.register(Contact)
